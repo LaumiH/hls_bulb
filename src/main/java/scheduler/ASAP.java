eed40b157e663bc6f1b1c2c8323f54dcda79f085
Laura Henning <laura-marie.henning@stud.h-da.de>
2021-11-20 13:39:50 +0100

    Add given project files from archive

---
+++ src/main/java/scheduler/ASAP.java
@@ -0,0 +1,57 @@
+package scheduler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ASAP extends Scheduler {
+
+    public Schedule schedule(final Graph sg) {
+        Map<Node, Interval> queue = new HashMap<>();
+        Map<Node, Interval> qq;
+        Map<Node, Interval> minslot = new HashMap<>();
+        Schedule schedule = new Schedule();
+
+        for (Node nd : sg) {
+            if (nd.root())
+                queue.put(nd, new Interval(0, nd.getDelay() - 1));
+        }
+        if(queue.size() == 0)
+            System.out.println("No root in Graph found. Empty or cyclic graph");
+
+        while (queue.size() > 0) {
+            qq = new HashMap<>();
+
+            for (Node nd : queue.keySet()) {
+                Interval slot = queue.get(nd);
+                schedule.add(nd, slot);
+
+                for (Node l : nd.successors()) {
+                    sg.handle(nd, l);
+
+                    Interval ii = minslot.get(l);
+                    if (ii == null)
+                        minslot.put(l, new Interval(slot.ubound + 1,
+                            slot.ubound + l.getDelay()));
+                    else if (ii.lbound.compareTo(slot.ubound) <= 0)
+                        minslot.put(l, new Interval(slot.ubound + 1,
+                            slot.ubound + l.getDelay()));
+
+                    if (!l.top())
+                        continue;
+                    ii = minslot.get(l);
+                    if ((queue.get(l) == null)) {
+                        if (qq.get(l) == null)
+                            qq.put(l, ii);
+                        else if (qq.get(l).lbound <= slot.ubound)
+                            qq.put(l, ii);
+                    } else if (queue.get(l).lbound <= slot.ubound)
+                        qq.put(l, ii);
+                }
+            }
+            queue = qq;
+        }
+        sg.reset();
+
+        return schedule;
+    }
+}
---
+++ src/main/java/scheduler/ResourceType.java
@@ -0,0 +1,67 @@
+package scheduler;
+
+public enum ResourceType {
+    MEM (2, 9.0, "Mem"),
+    ADD (1, 1.0, "Add"),
+    SUB (1, 1.4, "Sub"),
+    MUL (4, 2.3, "Mul"),
+    DIV (18, 4.3, "Div"),
+    SH (1, 2.0, "Shift"),
+    AND (1, 2.0, "And"),
+    OR (1, 2.0, "Or"),
+    CMP (1, 2.1, "Cmp"),
+    OTHER (1, 1.0, "Other"),
+    SLACK(1, 0.0, "Slack");
+
+    /**
+     * Delay (duration) of this resource type
+     */
+    public final Integer delay;
+
+    /**
+     * Weight of this resource type
+     */
+    public final Double weight;
+
+    /**
+     * Name of this resource type
+     */
+    public final String name;
+
+    ResourceType(Integer delay, Double weight, String name) {
+        this.delay = delay;
+        this.weight = weight;
+        this.name = name;
+    }
+
+    /**
+     * Get the resource type for the given string.
+     * @param id - the ID of the node to get the resource type from
+     * @return the resource type. ResourceType.OTHER if none was found
+     */
+    public static ResourceType getResourceType(String id){
+        if (id.contains("MUL")) {
+            return ResourceType.MUL;
+        } else if (id.contains("ADD")|| id.contains("INC")) {
+            return ResourceType.ADD;
+        } else if (id.contains("DIV")) {
+            return ResourceType.DIV;
+        } else if (id.contains("SUB")) {
+            return ResourceType.SUB;
+        } else if (id.contains("SH")) {
+            return ResourceType.SH;
+        } else if (id.contains("AND")) {
+            return ResourceType.AND;
+        } else if (id.contains("MEM")) {
+            return ResourceType.MEM;
+        } else if (id.contains("STORE") || id.contains("LOAD")) {
+            return ResourceType.MEM;
+        } else if (id.contains("OR")) {
+            return ResourceType.OR;
+        } else if (id.contains("IF") || id.contains("CMP")) {
+            return ResourceType.CMP;
+        } else {
+            return ResourceType.OTHER;
+        }
+    }
+}
