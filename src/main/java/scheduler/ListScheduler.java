fdef3bedb6e534268431cc1f90f04c808f94e38c
Derc42 <Dirk.Dippner@hotmail.de>
2021-12-08 16:27:03 +0100

    Updated ListScheduler Algorithm with res freeing

--- src/main/java/scheduler/ListScheduler.java
+++ src/main/java/scheduler/ListScheduler.java
@@ -2,46 +2,48 @@ package scheduler;
 
 import java.util.*;
 
-public class ListScheduler  {
+public class ListScheduler {
 
-    public Schedule schedule(final Graph sourceGraph, ResourceConstraint alpha){
+    public Schedule schedule(final Graph sourceGraph, ResourceConstraint alpha) {
         int t = 0;
         boolean all_nodes_scheduled = false;
-        boolean free_resources =  true; // all resources used
-        Map<Integer,List<Node>> priority_sorted_list = new TreeMap<>();
-        Map<Node,Integer> scheduled_list = new HashMap<>();
+        boolean free_resources = true; // all resources used
+        Map<Integer, List<Node>> priority_sorted_list = new TreeMap<>();
+        // Map<Node,Integer> scheduled_list = new HashMap<>();
         Schedule schedule = new Schedule();
         int lmax = 0; // need to set value?
         ResourceConstraint res_used = new ResourceConstraint(); // currently working
 
-        List<Node> nodes ;
+        List<Node> nodes;
         int nmbr_of_successors = 0;
-        for(Node nd : sourceGraph){ // Sort the nodes after number of successors
+        for (Node nd : sourceGraph) { // Sort the nodes after number of successors
 
             nmbr_of_successors = nd.allSuccessors().size();
             nodes = priority_sorted_list.get(nmbr_of_successors);
 
-            if(nodes == null){
-               System.out.println("Empty List with priority : "+nmbr_of_successors);
-               nodes = new ArrayList<Node>();
-            }else
-            {
-                System.out.println("Added Node to key : "+nmbr_of_successors);
+            if (nodes == null) {
+                System.out.println("Empty List with priority : " + nmbr_of_successors);
+                nodes = new ArrayList<Node>();
+            } else {
+                System.out.println("Added Node to key : " + nmbr_of_successors);
             }
             nodes.add(nd);
 
-            priority_sorted_list.put(-nmbr_of_successors,nodes); // negative cause list only gives set in ascending order
+            priority_sorted_list.put(-nmbr_of_successors, nodes); // negative cause list only gives set in ascending order
 
         }
 
         ResourceType needed_res = null;
         ResourceType res_to_check = null;
         Set<String> constraint_res_types = alpha.getAllRes().keySet(); // all res constraint types
-        Interval  ii = null;
+        Interval ii = null;
+        List<Node> curr_working_nodes = new ArrayList<Node>();
+        Node curr_working_node_w_min_delay = null;
+        int min_delay = Integer.MAX_VALUE;
         do {
             //  check which restype is free
             // check which node with the highest priority can use it
-            for(String type : constraint_res_types) {
+            for (String type : constraint_res_types) {
                 res_to_check = ResourceType.valueOf(type);
 
                 // check if res has free spot
@@ -58,6 +60,14 @@ public class ListScheduler  {
                                     // schedule node
                                     ii = new Interval(t, t + nd.getDelay());
                                     schedule.add(nd, ii, needed_res.name);
+
+                                    if (nd.getDelay() < min_delay) { //keeping track of the node which finishes first
+                                        min_delay = ii.length();
+                                        curr_working_node_w_min_delay = nd;
+                                    }
+
+                                    curr_working_nodes.add(nd);
+
                                     priority_sorted_list.get(key).remove(nd); // remove object from list
                                 }
                         }
@@ -66,22 +76,30 @@ public class ListScheduler  {
             }
             // checked all resources to schedule
 
+            // check if min delay is minimum after second round
+            for (Node nd : curr_working_nodes) {
+                if (nd.getDelay() < min_delay) {
+                    curr_working_node_w_min_delay = nd;
+                }
+            }
 
 
+            t = t + min_delay;// increase the time step counter
 
 
-            t = t + 1; // increase the time step counter
-            // and waiting until one res is free
-            for(Integer key : priority_sorted_list.keySet()){ // check if all nodes have been scheduled;
+            curr_working_nodes.remove(curr_working_node_w_min_delay); // removing node
+
+
+            for (Integer key : priority_sorted_list.keySet()) { // check if all nodes have been scheduled;
                 nodes = priority_sorted_list.get(key);
-                if(nodes.isEmpty()){
+                if (nodes.isEmpty()) {
                     all_nodes_scheduled = true;
-                }else{
-                    all_nodes_scheduled =  false;
+                } else {
+                    all_nodes_scheduled = false;
                     break;
                 }
             }
-        }while(!all_nodes_scheduled);
+        } while (!all_nodes_scheduled);
         // ready list
         // evaluate set of nodes that are ready to be executed
         // Set of candidates are the nodes which predecessor finished
@@ -93,8 +111,8 @@ public class ListScheduler  {
         // s + G < alpha(r) (number of schedulable operations) + (busy resources) <= available resources
         // set time step for all scheduled nodes
 
-            return schedule;
-        }
+        return schedule;
+    }
 
 
 }
