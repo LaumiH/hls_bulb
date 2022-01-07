9313f90b9b2578154f4349294ce5af53781d50bf
Laura Henning <laura-marie.henning@stud.tu-darmstadt.de>
2022-01-07 16:45:16 +0100

    add parameters

--- src/main/java/bulb/BulbGraph.java
+++ src/main/java/bulb/BulbGraph.java
@@ -1,16 +1,21 @@
 package bulb;
 
+import java.util.List;
 import java.util.Set;
 
 public class BulbGraph {
 
-    private final Set<BulbNode> nodes;
+    private final List<BulbNode> nodes;
 
     private BulbNode root;
 
     private int inspectedNodes = 0;
 
-    public BulbGraph(Set<BulbNode> nodes) {
+    private boolean lowerEqualsUpperReached = false;
+
+    private String parameters;
+
+    public BulbGraph(List<BulbNode> nodes) {
         this.nodes = nodes;
     }
 
@@ -29,10 +34,28 @@ public class BulbGraph {
     }
 
     public void print() {
-        System.out.printf("BULB tree contains %d nodes%n", this.nodes.size());
-        System.out.printf("%d nodes were further inspected, %d nodes violate resource constraints%n",
-                this.inspectedNodes, this.nodes.size() - this.inspectedNodes);
+        System.out.printf("BULB tree contains %d nodes%n", this.inspectedNodes);
+        System.out.printf("%d tried combinations violated resource constraints%n",
+                this.nodes.size() - this.inspectedNodes);
+        System.out.println("Lower == Upper reached: " + lowerEqualsUpperReached);
+        System.out.println("Parameters: " + parameters);
         System.out.println("Print BULB tree:");
         System.out.println(this.root.toString());
+    }
+
+    public boolean isLowerEqualsUpperReached() {
+        return lowerEqualsUpperReached;
+    }
+
+    public void setLowerEqualsUpperReached(boolean lowerEqualsUpperReached) {
+        this.lowerEqualsUpperReached = lowerEqualsUpperReached;
+    }
+
+    public String getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(String parameters) {
+        this.parameters = parameters;
     }
 }
