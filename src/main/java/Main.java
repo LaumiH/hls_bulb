5937d3cf09c2aef7d41a5a199d11eee4b6c4d486
Laura Henning <laura-marie.henning@stud.tu-darmstadt.de>
2022-01-09 21:14:44 +0100

    Improve metric printout, add possibility to run with command line args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import bulb.BULB;
-import bulb.BULBException;
 import bulb.BulbGraph;
 import scheduler.*;
 
@@ -18,15 +17,32 @@ public class Main {
 
         System.out.println("START OF PROGRAM");
 
-        //String folderGraphs = "graphs";
-        String folderGraphs = "BULB_resources/g";
-        File folder = new File(folderGraphs);
-        List<File> listOfGraphFiles = List.of(Objects.requireNonNull(folder.listFiles()));
+        //this is to keep track of overall statistics in the benchmark
+        int completed = 0;
+        Map<String, String> timeouted = new HashMap<>();     //filename, parameters
+        List<String> notEqual = new ArrayList<>();    //filename, res and parameters of both
 
-        //String folderResources = "resources/vlsi";
+        List<File> listOfGraphFiles = new ArrayList<>();
+        List<File> listOfResFiles = new ArrayList<>();
+        String folderGraphs = "graphs";
         String folderResources = "BULB_resources/r";
-        folder = new File(folderResources);
-        List<File> listOfResFiles = List.of(Objects.requireNonNull(folder.listFiles()));
+        //String folderGraphs = "BULB_resources/g";
+        //String folderResources = "resources/vlsi";
+
+        if (args.length > 0) {
+            File dfg = new File(args[0]);
+            File res = new File(args[1]);
+            listOfGraphFiles.add(dfg);
+            listOfResFiles.add(res);
+        } else {
+            File folder = new File(folderGraphs);
+            File[] files = folder.listFiles();
+            Arrays.sort(Objects.requireNonNull(files)); //sort alphabetically
+            listOfGraphFiles = List.of(files);
+
+            folder = new File(folderResources);
+            listOfResFiles = List.of(Objects.requireNonNull(folder.listFiles()));
+        }
 
         // Store current System.out before assigning a new value
         PrintStream console = System.out;
@@ -39,12 +55,12 @@ public class Main {
                 String dotFilePath = folderGraphs + "/" + dotFile;
                 String resFilePath = folderResources + "/" + resFile;
 
-                String logDir = "target/" + dotFile.replaceAll(".dot", "");
+                String logDir = "benchmark/" + dotFile.replaceAll(".dot", "");
                 String logName = logDir + "/" + dotFile.replaceAll(".dot", "") + "__" + resFile + ".txt";
 
                 // Creating a File object that represents the log file
                 File logFile = new File(logDir);
-                if (! logFile.exists()){
+                if (!logFile.exists()) {
                     if (!logFile.mkdirs()) {
                         System.out.printf("Cannot create directory %s%n", logFile);
                         System.exit(-1);
@@ -52,14 +68,12 @@ public class Main {
                     // If you require it to make the entire directory path including parents,
                     // use directory.mkdirs(); here instead.
                 }
-                logFile = new File(logName);
                 PrintStream o = null;
                 try {
-                    o = new PrintStream(logFile);
+                    o = new PrintStream(logName);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
-                } finally {
-                    Objects.requireNonNull(o).close();
+                    System.exit(-1);
                 }
 
                 System.setOut(console);
@@ -80,7 +94,7 @@ public class Main {
 
                 System.out.printf("PARSING INPUT GRAPH: %s%n", dotFilePath);
                 Graph g = dr.parse(dotFilePath);
-                System.out.printf("Input graph:%n%n%s%n", g.diagnose());
+                //System.out.printf("Input graph:%n%n%s%n", g.diagnose());
 
                 System.out.println("DOING ASAP SCHEDULE");
                 Scheduler s = new ASAP();
@@ -119,37 +133,41 @@ public class Main {
 
                 List<String> lBoundEstimators = new ArrayList<>(Arrays.asList("ASAP", "PAPER"));
                 List<Boolean> lazyALAP = new ArrayList<>(Arrays.asList(false, true));
+                List<Boolean> updateAlapBound = new ArrayList<>(Arrays.asList(true, false));
 
                 System.setOut(console);
                 for (String lBoundEstimator : lBoundEstimators) {
                     for (boolean lazy : lazyALAP) {
-                        System.out.printf("%n%n%nSTARTING BULB SCHEDULE with %n" +
-                                        "\t%s lower bound estimator,%n" +
-                                        "\tlist scheduler upper bound estimator,%n" +
-                                        "\t%s alap schedule in enumerate for loop%n%n",
-                                lBoundEstimator, lazy ? "lazy" : "normal");
-                        BULB bulbScheduler = new BULB(g, lBoundEstimator, "LIST", rc, asap, normal_alap, lazy_alap, lazy);
-                        ExecutorService exs = Executors.newSingleThreadExecutor();
-                        //List<Future<Schedule>> bulb2F =
-                        //exs.invokeAll(List.of(new BULB(g, "ASAP", "LIST", rc, asap, normal_alap, lazy_alap, false)), 2, TimeUnit.SECONDS);
-                        //        exs.invokeAll(List.of(new BULB(g, "ASAP", "LIST", rc, asap, normal_alap, lazy_alap, false)), 2, TimeUnit.SECONDS);
+                        for (boolean updateAlap : updateAlapBound) {
+                            System.out.printf("########################################" +
+                                            "########################################" +
+                                            "%nSTARTING BULB SCHEDULE with %s estimator, %s alap, updateAlapBound=%s%n%n",
+                                    lBoundEstimator, lazy ? "lazy" : "normal", updateAlap ? "true" : "false");
+                            BULB bulbScheduler = new BULB(g, lBoundEstimator, rc, asap, normal_alap, lazy_alap, lazy, updateAlap);
+                            ExecutorService exs = Executors.newSingleThreadExecutor();
+                            //List<Future<Schedule>> bulb2F =
+                            //exs.invokeAll(List.of(new BULB(g, "ASAP", "LIST", rc, asap, normal_alap, lazy_alap, false)), 2, TimeUnit.SECONDS);
+                            //        exs.invokeAll(List.of(new BULB(g, "ASAP", "LIST", rc, asap, normal_alap, lazy_alap, false)), 2, TimeUnit.SECONDS);
 
-                        Future<Schedule> future = exs.submit(bulbScheduler);
-                        Schedule bulb = new Schedule();
-                        try {
-                            bulb = future.get(5, TimeUnit.SECONDS);
-                            bulbSchedules.put(bulbScheduler.getBulbGraph(), bulb);
-                        } catch (Exception e) {
-                            System.out.println("meow");
-                            e.printStackTrace();
-                        } finally {
-                            future.cancel(true); //this method will stop the running underlying task
-                            while(!future.isDone()) {
-                                System.out.println("Waiting for completion of thread");
+                            Future<Schedule> future = exs.submit(bulbScheduler);
+                            Schedule bulb;
+                            try {
+                                bulb = future.get(1, TimeUnit.HOURS);
+                                bulbSchedules.put(bulbScheduler.getBulbGraph(), bulb);
+                                completed++;
+                            } catch (Exception e) {
+                                System.out.println("Timeout reached while computing " + dotFile + " with " + resFile);
+                                String alap = lazy ? "lazy":"normal";
+                                timeouted.put(logName, lBoundEstimator + " estimator, " + alap + "alap, updateAlapBound=" + updateAlap);
+                            } finally {
+                                future.cancel(true); //this method will stop the running underlying task
+                                while (!future.isDone()) {
+                                    System.out.println("Waiting for completion of thread");
+                                }
+                                exs.shutdownNow();
+                                //bulb.diagnose(rc, g.size());
+                                Thread.sleep(1000);
                             }
-                            exs.shutdownNow();
-                            bulb.diagnose(rc, g.size());
-                            Thread.sleep(5 * 1000);
                         }
                     }
                 }
@@ -170,15 +188,18 @@ public class Main {
                 System.out.println("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
 
                 Map<BulbGraph, Schedule> copy = new HashMap<>(bulbSchedules);
-
                 Iterator<Map.Entry<BulbGraph, Schedule>> iterator = copy.entrySet().iterator();
+                boolean allEqual = true;
                 while (iterator.hasNext()) {
                     Map.Entry<BulbGraph, Schedule> next = iterator.next();
                     iterator.remove();
                     for (Map.Entry<BulbGraph, Schedule> e : copy.entrySet()) {
                         System.out.printf("Comparing %s %n\t\t with %s%n%n--->  ", next.getKey().getParameters(), e.getKey().getParameters());
                         if (!next.getValue().compare(e.getValue())) {
-                            throw new BULBException("Schedules differ");
+                            System.out.println(next.getValue().diagnose(rc, g.size()));
+                            System.out.println(e.getValue().diagnose(rc, g.size()));
+                            allEqual = false;
+                            notEqual.add(logName + ": " + next.getKey().getParameters() + " with " + e.getKey().getParameters());
                         }
                         System.out.println();
                         System.out.println(next.getKey().printMetrics());
@@ -192,12 +213,14 @@ public class Main {
                 System.out.println("Printing schedules");
                 System.out.println("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
 
+                if (allEqual) {
+                    System.out.println(bulbSchedules.values().iterator().next().diagnose(rc, g.size()));
+                }
+
                 for (Map.Entry<BulbGraph, Schedule> e : bulbSchedules.entrySet()) {
                     System.out.println(e.getKey().printMetrics());
                     System.out.println(e.getKey().print());
                 }
-
-                System.out.println(bulbSchedules.values().iterator().next().diagnose(rc, g.size()));
 
                 System.setOut(console);
                 System.out.printf("Completed %s on %s%n%n%n", dotFile, resFile);
@@ -208,6 +231,34 @@ public class Main {
         }
 
         System.setOut(console);
+        System.out.println("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+
+        PrintStream o = null;
+        try {
+            o = new PrintStream("benchmarks/metrics.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        System.setOut(o);
+
+        System.out.printf("%d tried combinations completed in given time%n", completed);
+        System.out.printf("%d tried combinations timed out%n", timeouted.size());
+        for (Map.Entry<String,String> t : timeouted.entrySet()) {
+            System.out.printf("\t- %s timed out with %s%n", t.getKey(), t.getValue());
+        }
+
+        System.out.printf("%n%n%d tried comparisons produced unequal schedules%n", notEqual.size());
+        Collections.sort(notEqual);
+        for (String s : notEqual) {
+            System.out.printf("%s produced unequal schedules%n", s);
+        }
+
+        System.setOut(console);
+
         System.out.println("\n\n\nEND OF PROGRAM");
     }
 }
